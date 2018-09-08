678750ca23947fc70a3185445126c894503dccb3
Wamm K. D <jaft.r@outlook.com>
2018-09-08 01:27:26 -0500

    Replace the port in the Nginx conf. file rather than Artanis's, now. And start Nginx.
    I meant to mention in the last commit but I might switch this over to
    the `passenger-docker` Dockfile, at some point. We'll see. But this
    works, both locally and remotely – now –, so that's good.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 ## Do whatever you need with env vars here ...
-sed -i -e 's/host.port = 3000/host.port = '"$PORT"'/g' /myapp/conf/artanis.conf
+sed -i -e 's/listen       80;/listen       '"$PORT"';/g' /etc/nginx/conf.d/default.conf
+
+/usr/sbin/nginx
 
 # Hand off to the CMD
 exec "$@"

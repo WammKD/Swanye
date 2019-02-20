e5b8343eef9f7ccccc5dcb22ceed0132332dd11e
Wamm K. D <jaft.r@outlook.com>
2019-02-20 00:32:18 -0600

    Update the `PEOPLE` table for more relevant usage. Namely, distinguish between the username and a nickname (`NAME`; I believe there was something regarding this in the ActivityPub spec.; I no longer remember), store E-mail, and track creation and confirmation token to allow users to confirm the existence of their accounts.
    I still need to handle if a username already exists (and maybe decide
    that only one E-mail per account but I'm still not sure on that one, yet).

--- db/migration/PEOPLE_20180910180518.scm
+++ db/migration/PEOPLE_20180910180518.scm
@@ -6,12 +6,15 @@
 (migrate-create
   (create-table
     'PEOPLE
-    '(ID                 auto       (#:primary-key))
-    '(NAME               char-field (#:maxlen  32  #:not-null #:unique))
-    '(PASSWORD           char-field (#:maxlen 500  #:not-null))
-    '(SALT               char-field (#:maxlen 500  #:not-null))
-    '(PREFERRED_USERNAME char-field (#:maxlen  32))
-    '(SUMMARY            char-field (#:maxlen 500))))
+    '(ID                 auto        (#:primary-key))
+    '(USERNAME           char-field  (#:maxlen  32  #:not-null #:unique))
+    '(E_MAIL             char-field  (#:maxlen 256  #:not-null))
+    '(PASSWORD           char-field  (#:maxlen 500  #:not-null))
+    '(SALT               char-field  (#:maxlen 256  #:not-null))
+    '(NAME               char-field  (#:maxlen  32  #:not-null #:default ""))
+    '(SUMMARY            char-field  (#:maxlen 500  #:not-null #:default ""))
+    '(CREATED_AT         big-integer               (#:not-null))
+    '(CONFIRMATION_TOKEN char-field  (#:maxlen 128  #:not-null #:unique))))
 (migrate-up
   (change-table
     'PEOPLE

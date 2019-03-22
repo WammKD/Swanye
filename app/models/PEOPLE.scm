fae49c258b0db76e4ea9dae1265588c1f6522924
Wamm K. D <jaft.r@outlook.com>
2019-03-22 00:13:59 -0500

    So _this_ is how you use models…

---
+++ app/models/PEOPLE.scm
@@ -0,0 +1,15 @@
+;; Model PEOPLE definition of myapp
+;; Please add your license header here.
+;; This file is generated automatically by GNU Artanis.
+(create-artanis-model   ; DO NOT REMOVE THIS LINE!!!
+  PEOPLE
+  (ID                 auto        (#:primary-key #:not-null #:unique))
+  (USERNAME           char-field  (#:maxlen  32  #:not-null #:unique))
+  (E_MAIL             char-field  (#:maxlen 256  #:not-null))
+  (PASSWORD           char-field  (#:maxlen 500  #:not-null))
+  (SALT               char-field  (#:maxlen 256  #:not-null))
+  (NAME               char-field  (#:maxlen  32  #:not-null #:default ""))
+  (SUMMARY            char-field  (#:maxlen 500  #:not-null #:default ""))
+  (CREATED_AT         big-integer               (#:not-null))
+  (CONFIRMATION_TOKEN char-field  (#:maxlen 128  #:not-null #:unique)))
+

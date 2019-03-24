70aedc271000d071f7ca028af9bffbe29c6a14ce
Wamm K. D <jaft.r@outlook.com>
2019-03-24 02:43:53 -0500

    Add in logging in at the beginning so I don't run into being denied pushing after waiting for so long for things to build.
    Also, grab the E-mail info. from the `.env` to properly setup
    E-mailing for the server.

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+heroku container:login
+
+
 ADDR=$(cut -d " " -f 2 <<< $(cut -d "=" -f 2 <<< $(grep "db.addr = "     conf/artanis.conf)))
 USER=$(cut -d " " -f 2 <<< $(cut -d "=" -f 2 <<< $(grep "db.username = " conf/artanis.conf)))
 PASS=$(cut -d " " -f 2 <<< $(cut -d "=" -f 2 <<< $(grep "db.passwd = "   conf/artanis.conf)))
 NAME=$(cut -d " " -f 2 <<< $(cut -d "=" -f 2 <<< $(grep "db.name = "     conf/artanis.conf)))
+
+EMAIL_ADDR=`cat .env | grep ADDRESS | cut -d "=" -f 2`
+EMAIL_PASS=`cat .env | grep PASSWOR | cut -d "=" -f 2`
 
 
 
@@ -11,6 +17,9 @@ sed -i -e 's/db.addr = '"$ADDR"'/db.addr = '"$SWANYE_DEPLOY_ADDR"'/g'         co
 sed -i -e 's/db.username = '"$USER"'/db.username = '"$SWANYE_DEPLOY_USER"'/g' conf/artanis.conf
 sed -i -e 's/db.passwd = '"$PASS"'/db.passwd = '"$SWANYE_DEPLOY_PASS"'/g'     conf/artanis.conf
 sed -i -e 's/db.name = '"$NAME"'/db.name = '"$SWANYE_DEPLOY_NAME"'/g'         conf/artanis.conf
+
+sed -i -e 's/"AuthUser=\$EMAIL_ADDRESS"/"AuthUser='"$EMAIL_ADDR"'"/g'  Dockerfile
+sed -i -e 's/"AuthPass=\$EMAIL_PASSWORD"/"AuthPass='"$EMAIL_PASS"'"/g' Dockerfile
 
 echo -e "\n\nCOPY entrypoint.sh /entrypoint.sh\nRUN chmod 755 /entrypoint.sh\n\nENTRYPOINT [\"/entrypoint.sh\"]\n" >> Dockerfile
 echo -n "CMD [\"/usr/local/bin/art\", \"work\"]"                                                                   >> Dockerfile
@@ -25,6 +34,9 @@ sed -i -e 's/db.addr = '"$SWANYE_DEPLOY_ADDR"'/db.addr = '"$ADDR"'/g'         co
 sed -i -e 's/db.username = '"$SWANYE_DEPLOY_USER"'/db.username = '"$USER"'/g' conf/artanis.conf
 sed -i -e 's/db.passwd = '"$SWANYE_DEPLOY_PASS"'/db.passwd = '"$PASS"'/g'     conf/artanis.conf
 sed -i -e 's/db.name = '"$SWANYE_DEPLOY_NAME"'/db.name = '"$NAME"'/g'         conf/artanis.conf
+
+sed -i -e 's/"AuthUser='"$EMAIL_ADDR"'"/"AuthUser=\$EMAIL_ADDRESS"/g'  Dockerfile
+sed -i -e 's/"AuthPass='"$EMAIL_PASS"'"/"AuthPass=\$EMAIL_PASSWORD"/g' Dockerfile
 
 head -n -7 Dockerfile > temp.txt; mv temp.txt Dockerfile
 perl -pi -e 'chomp if eof' Dockerfile

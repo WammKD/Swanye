c9b2ef3eaa9380ccd0ab72e51365e288e574c1dc
Wamm K. D <jaft.r@outlook.com>
2019-03-28 20:11:00 -0500

    I don't know if I'll end up making any more endpoints and I'm not, currently, making any use of the `define-` function but, organizationally, I think it makes sense to have these in a controller. Also add the public key into the mix.

---
+++ app/controllers/users.scm
@@ -0,0 +1,48 @@
+;; Controller users definition of Swanye
+;; Please add your license header here.
+;; This file is generated automatically by GNU Artanis.
+(define-artanis-controller users) ; DO NOT REMOVE THIS LINE!!!
+
+(use-modules (app models PEOPLE) (web request))
+
+(get "/users/:user" (lambda (rc)
+                      (redirect-to
+                        rc
+                        (string-append/shared "/@" (params rc "user"))
+                        #:status 200)))
+
+(get "/@:user" #:mime 'json #:conn #t
+  (lambda (rc)
+    (let ([poss ($PEOPLE 'get #:columns   '(*)
+                              #:condition (where #:USERNAME (params rc "user")))])
+      (if (null? poss)
+          (redirect-to rc "/404")
+        (let* ([request                  (rc-req rc)]
+               [accept      (request-accept request)]
+               [user                      (car poss)]
+               [username (assoc-ref user "USERNAME")])
+          (if (or
+                (assoc-ref accept 'application/activity+json)
+                (let ([ld (assoc-ref accept 'application/ld+json)])
+                  (and ld (assoc-ref ld 'profile) (string=?
+                                                    (assoc-ref ld 'profile)
+                                                    "https://www.w3.org/ns/activitystreams"))))
+              (let ([userURL (string-append/shared
+                               "https://" (car (request-host request))
+                               "/users/"  username)])
+                (:mime rc `(("@context"          . ("https://www.w3.org/ns/activitystreams"
+                                                    "https://w3id.org/security/v1"))
+                            ("id"                . ,userURL)
+                            ("type"              . "Person")
+                            ("preferredUsername" . ,(assoc-ref user "NAME"))
+                            ("inbox"             . ,(string-append/shared
+                                                      userURL
+                                                      "/inbox"))
+                            ("publicKey"         . (("id"           . ,(string-append/shared
+                                                                         userURL
+                                                                         "#main-key"))
+                                                    ("owner"        . ,userURL)
+                                                    ("publicKeyPem" . ,(assoc-ref
+                                                                         user
+                                                                         "PUBLIC")))))))
+            (string-append/shared "The user page of " username "!")))))))

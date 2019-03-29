52a136b609ef14212f01419197d7e381d20003de
Wamm K. D <jaft.r@outlook.com>
2019-03-28 22:50:05 -0500

    And adjust the actor to decrypt the new representation and still use it.

--- app/controllers/users.scm
+++ app/controllers/users.scm
@@ -3,7 +3,8 @@
 ;; This file is generated automatically by GNU Artanis.
 (define-artanis-controller users) ; DO NOT REMOVE THIS LINE!!!
 
-(use-modules (app models PEOPLE) (web request))
+(use-modules (app       models   PEOPLE) (ice-9 eval-string)
+             (industria crypto blowfish) (rnrs  bytevectors) (web request))
 
 (get "/users/:user" (lambda (rc)
                       (redirect-to
@@ -27,9 +28,17 @@
                   (and ld (assoc-ref ld 'profile) (string=?
                                                     (assoc-ref ld 'profile)
                                                     "https://www.w3.org/ns/activitystreams"))))
-              (let ([userURL (string-append/shared
-                               "https://" (car (request-host request))
-                               "/users/"  username)])
+              (let ([userURL         (string-append/shared
+                                       "https://" (car (request-host request))
+                                       "/users/"  username)]
+                    [publicEncrypted (eval-string (assoc-ref user "PUBLIC"))])
+                (blowfish-decrypt!
+                  publicEncrypted 0
+                  publicEncrypted 0
+                  (reverse-blowfish-schedule
+                    (eval-string
+                      (get-string-all-with-detected-charset "/myapp/.key"))))
+
                 (:mime rc `(("@context"          . ("https://www.w3.org/ns/activitystreams"
                                                     "https://w3id.org/security/v1"))
                             ("id"                . ,userURL)
@@ -42,7 +51,6 @@
                                                                          userURL
                                                                          "#main-key"))
                                                     ("owner"        . ,userURL)
-                                                    ("publicKeyPem" . ,(assoc-ref
-                                                                         user
-                                                                         "PUBLIC")))))))
+                                                    ("publicKeyPem" . ,(utf8->string
+                                                                         publicEncrypted)))))))
             (string-append/shared "The user page of " username "!")))))))

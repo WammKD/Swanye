889ac078f5b01b61cbdd05736b7cff3dd5f02d67
Wamm K. D <jaft.r@outlook.com>
2019-04-21 17:40:04 -0500

    Turns out that JawsDB doesn't like such long `varchar`s for primary keys so we've had to revert to this.

--- app/models/FOLLOWERS.scm
+++ app/models/FOLLOWERS.scm
@@ -3,6 +3,6 @@
 ;; This file is generated automatically by GNU Artanis.
 (create-artanis-model   ; DO NOT REMOVE THIS LINE!!!
   FOLLOWERS
-  (FOLLOWEE big-integer (#:not-null))
-  (FOLLOWER char-field  (#:not-null #:maxlen 3008))
-  #:primary-keys (FOLLOWEE FOLLOWER))
+  (ID       auto        (#:primary-key #:not-null #:unique))
+  (FOLLOWEE big-integer               (#:not-null))
+  (FOLLOWER text                      (#:not-null)))

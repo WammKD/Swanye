5ce328136a83961c3a2842d774037fea96df9976
Sherlock Holmes <jaft.r@outlook.com>
2019-10-26 16:19:16 -0500

    Adjust this for using `msmtp` instead of `ssmtp`, now.

--- deploy.sh
+++ deploy.sh
@@ -18,8 +18,9 @@ sed -i -e 's/db.username = '"$USER"'/db.username = '"$SWANYE_DEPLOY_USER"'/g' co
 sed -i -e 's/db.passwd = '"$PASS"'/db.passwd = '"$SWANYE_DEPLOY_PASS"'/g'     conf/artanis.conf
 sed -i -e 's/db.name = '"$NAME"'/db.name = '"$SWANYE_DEPLOY_NAME"'/g'         conf/artanis.conf
 
-sed -i -e 's/"AuthUser=\$EMAIL_ADDRESS"/"AuthUser='"$EMAIL_ADDR"'"/g'  Dockerfile
-sed -i -e 's/"AuthPass=\$EMAIL_PASSWORD"/"AuthPass='"$EMAIL_PASS"'"/g' Dockerfile
+sed -i -e 's/"from           \$EMAIL_ADDRESS"/"from           '"$EMAIL_ADDR"'"/g'  Dockerfile
+sed -i -e 's/"user           \$EMAIL_ADDRESS"/"user           '"$EMAIL_ADDR"'"/g'  Dockerfile
+sed -i -e 's/"password       \$EMAIL_PASSWORD"/"password       '"$EMAIL_PASS"'"/g' Dockerfile
 
 echo -e "\n\nCOPY entrypoint.sh /entrypoint.sh\nRUN chmod 755 /entrypoint.sh\n\nENTRYPOINT [\"/entrypoint.sh\"]\n" >> Dockerfile
 echo -n "CMD [\"/usr/local/bin/art\", \"work\"]"                                                                   >> Dockerfile
@@ -35,8 +36,9 @@ sed -i -e 's/db.username = '"$SWANYE_DEPLOY_USER"'/db.username = '"$USER"'/g' co
 sed -i -e 's/db.passwd = '"$SWANYE_DEPLOY_PASS"'/db.passwd = '"$PASS"'/g'     conf/artanis.conf
 sed -i -e 's/db.name = '"$SWANYE_DEPLOY_NAME"'/db.name = '"$NAME"'/g'         conf/artanis.conf
 
-sed -i -e 's/"AuthUser='"$EMAIL_ADDR"'"/"AuthUser=\$EMAIL_ADDRESS"/g'  Dockerfile
-sed -i -e 's/"AuthPass='"$EMAIL_PASS"'"/"AuthPass=\$EMAIL_PASSWORD"/g' Dockerfile
+sed -i -e 's/"from           '"$EMAIL_ADDR"'"/"from           \$EMAIL_ADDRESS"/g'  Dockerfile
+sed -i -e 's/"user           '"$EMAIL_ADDR"'"/"user           \$EMAIL_ADDRESS"/g'  Dockerfile
+sed -i -e 's/"password       '"$EMAIL_PASS"'"/"password       \$EMAIL_PASSWORD"/g' Dockerfile
 
 head -n -7 Dockerfile > temp.txt; mv temp.txt Dockerfile
 perl -pi -e 'chomp if eof' Dockerfile

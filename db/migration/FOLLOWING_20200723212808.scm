60a0006baf3a949c199b8a4004e7bd8b0c805ef3
Sherlock Holmes <Wamm_KD_Schmelingski@yahoo.com>
2020-07-23 22:51:46 -0500

    Similar concept; Artanis add `#:primary-key` and `#:not-null` to `auto` so let's just not include them.

---
+++ db/migration/FOLLOWING_20200723212808.scm
@@ -0,0 +1,16 @@
+;; Migration FOLLOWING_20200723212808 definition of Swanye
+;; Please add your license header here.
+;; This file is generated automatically by GNU Artanis.
+(create-artanis-migration FOLLOWING_20200723212808) ; DO NOT REMOVE THIS LINE!!!
+
+(migrate-create
+  (create-table
+    'FOLLOWING
+    '(ID       auto        (#:unique))
+    '(FOLLOWER big-integer (#:not-null))
+    '(FOLLOWEE text        (#:not-null #:maxlen 60000))
+    '(PENDING  boolean     (#:not-null))))
+(migrate-up
+  (display "Add your up code\n"))
+(migrate-down
+  (drop-table 'FOLLOWING))

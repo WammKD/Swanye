59f12aab249c5b09a4b8d0da1bc1648236dae6dd
Sherlock Holmes <Wamm_KD_Schmelingski@yahoo.com>
2020-07-24 18:35:02 -0500

    Kick things off, slowly. I'm hoping that adding some "heading" comments may help to bring a bit more clear organization to the files than there was, originally; at least, unless I come up with better solutions.

--- app/controllers/auth.scm
+++ app/controllers/auth.scm
@@ -3,3 +3,19 @@
 ;; This file is generated automatically by GNU Artanis.
 (define-artanis-controller auth) ; DO NOT REMOVE THIS LINE!!!
 
+(use-modules (app models PEOPLE)
+             ((artanis utils) #:select (get-random-from-dev
+                                        get-string-all-with-detected-charset)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  U T I L I T I E S  ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;
+(define (SALTER password saltString)
+  (string->sha-512 (string-append password saltString)))
+
+(define (bv->string bv)
+  (string-append/shared (fold
+                          (lambda (int final)
+                            (string-append final " " (number->string int)))
+                          "#vu8("
+                          (bytevector->u8-list bv))                         ")"))

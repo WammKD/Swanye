b8f7250c389937d58ae053ac65a98dd39fdaf2ab
Sherlock Holmes <Wamm_KD_Schmelingski@yahoo.com>
2020-07-24 15:03:11 -0500

    Interestingly, this is how I found things in `master`; I wonder if I'd started making some of the changes that I have to make now and came to the same conclusions.
    `PEOPLE`, in both the migration and the model, are still the old
    stuff, though. So who knows.

---
+++ app/models/FOLLOWERS.scm
@@ -0,0 +1,9 @@
+;; Model FOLLOWERS definition of Swanye
+;; Please add your license header here.
+;; This file is generated automatically by GNU Artanis.
+(create-artanis-model   ; DO NOT REMOVE THIS LINE!!!
+  FOLLOWERS
+  (ID       auto        (#:unique))
+  (FOLLOWEE big-integer (#:not-null))
+  (FOLLOWER text        (#:not-null))
+

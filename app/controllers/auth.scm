e85f0ac7b87a7b8a691f7b63089c2132b5a674c6
Sherlock Holmes <Wamm_KD_Schmelingski@yahoo.com>
2020-07-24 15:38:56 -0500

    And so we begin.

---
+++ app/controllers/auth.scm
@@ -0,0 +1,5 @@
+;; Controller auth definition of Swanye
+;; Please add your license header here.
+;; This file is generated automatically by GNU Artanis.
+(define-artanis-controller auth) ; DO NOT REMOVE THIS LINE!!!
+

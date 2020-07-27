fa053eaf58d5f53ae69014240f342f79d23bcb2f
Sherlock Holmes <Wamm_KD_Schmelingski@yahoo.com>
2020-07-27 18:48:57 -0500

    Be able to transfer to the dashboard.

---
+++ app/controllers/main.scm
@@ -0,0 +1,8 @@
+;; Controller main definition of Swanye
+;; Please add your license header here.
+;; This file is generated automatically by GNU Artanis.
+(define-artanis-controller main) ; DO NOT REMOVE THIS LINE!!!
+
+(get "/" #:with-auth "/auth/sign_in"
+  (lambda (rc)
+    (view-render "dashboard" (the-environment))))

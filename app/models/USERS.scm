2a72dcd045a8e2ab5cf94d2591b5aab8a6311590
Sherlock Holmes <Wamm_KD_Schmelingski@yahoo.com>
2020-08-09 15:26:12 -0500

    It made sense to name the table PEOPLE when I was imagining that all Actors (since we wouldn't be storing remote Actors) would be stored there but I think User is a better descriptor for person-on-this-instance when compared against the ActivityPub term Actor; people immediately think of "users of the system" when they hear "user" while "people"/"person" is more vague.

--- app/models/USERS.scm
+++ app/models/USERS.scm
@@ -1,9 +1,9 @@
-;; Model PEOPLE definition of Swanye
+;; Model USERS definition of Swanye
 ;; Please add your license header here.
 ;; This file is generated automatically by GNU Artanis.
 (create-artanis-model   ; DO NOT REMOVE THIS LINE!!!
-  PEOPLE
-  (ID                 auto        (#:primary-key  #:not-null #:unique))
+  USERS
+  (USER_ID            auto        (#:primary-key  #:not-null #:unique))
   (ACTOR_ID           big-integer                (#:not-null #:unique))
   (USERNAME           char-field  (#:maxlen    64 #:not-null #:unique))
   (E_MAIL             char-field  (#:maxlen   255 #:not-null))
--- db/migration/USERS_20200722223253.scm
+++ db/migration/USERS_20200722223253.scm
@@ -1,12 +1,12 @@
-;; Migration PEOPLE_20200722223253 definition of Swanye
+;; Migration USERS_20200722223253 definition of Swanye
 ;; Please add your license header here.
 ;; This file is generated automatically by GNU Artanis.
-(create-artanis-migration PEOPLE_20200722223253) ; DO NOT REMOVE THIS LINE!!!
+(create-artanis-migration USERS_20200722223253) ; DO NOT REMOVE THIS LINE!!!
 
 (migrate-create
   (create-table
-    'PEOPLE
-    '(ID                 auto        (#:primary-key  #:not-null #:unique))
+    'USERS
+    '(USER_ID            auto        (#:primary-key  #:not-null #:unique))
     '(ACTOR_ID           big-integer                (#:not-null #:unique))
     '(USERNAME           char-field  (#:maxlen    64 #:not-null #:unique))
     '(E_MAIL             char-field  (#:maxlen   255 #:not-null))
@@ -18,9 +18,9 @@
     '(PRIVATE_KEY        text                       (#:not-null))))
 (migrate-up
   (change-table
-    'PEOPLE
+    'USERS
     '((NAME     "test")
       (PASSWORD "cd56f562179949664713674d99f59ebf52810e4c655465a2fb01be20d5218d7712f2039db3cfdd2e8249cb876ced52b426b201f577181e455e4cd461057b8365")
       (SALT     "8071f9e536662df8f58de2342158398b33b54cc07298b9f01bd19eb63c5668f198a54d5586a94ac711ffad7cd14a09cc0cb0186c702a2effddc287bbf08ec1b660a28789d9fd60e3e70ac31fcba6bf415427ac6b5045912d1f6ad04694d236f53af7f81daa9a8700056b58b218a53d4207a4f3089e4766ed8231f0556515b16c"))))
 (migrate-down
-  (drop-table 'PEOPLE))
+  (drop-table 'USERS))

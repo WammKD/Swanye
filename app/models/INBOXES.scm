fb36c42e4f0dbaf66a9c6de8fcd2e1d616198390
Sherlock Holmes <Wamm_KD_Schmelingski@yahoo.com>
2020-09-22 23:47:15 -0500

    Might as well store the related ACTOR and OBJECT IDs in our databases to make it easier to look those up.
    I'm sure I'll have to cleanup the code that handles this but I think
    it makes sense.

--- app/models/INBOXES.scm
+++ app/models/INBOXES.scm
@@ -3,8 +3,10 @@
 ;; This file is generated automatically by GNU Artanis.
 (create-artanis-model   ; DO NOT REMOVE THIS LINE!!!
   INBOXES
-  (ID       auto        (#:unique))
-  (USER_ID  big-integer (#:not-null))
-  (ACTIVITY text        (#:not-null))
-  (TYPE     text        (#:not-null)))
+  (       ID auto        (#:unique))
+  (  USER_ID big-integer (#:not-null))
+  ( ACTOR_ID big-integer (#:not-null))
+  (OBJECT_ID big-integer (#:not-null))
+  (ACTIVITY  text        (#:not-null))
+  (TYPE      text        (#:not-null)))
 

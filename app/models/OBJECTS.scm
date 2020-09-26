23c5228ce1f753f2eba48a21c641b0f241e943e6
Sherlock Holmes <Wamm_KD_Schmelingski@yahoo.com>
2020-09-26 03:51:51 -0500

    This should cover all the available attributes for Objects (with the intention to properly implement these at a later date).
    I think I'm going to keep the Actors in the `OBJECTS` table.
    
    Since there's so much overlap, we can cover all the inherited Object
    attributes in `OBJECTS` and put Actor-specific attributes in the
    `ACTORS` table (we can probably remove the `AP_ID` and `ACTOR_TYPE` to
    accomplish that fully but we'll do that once we're able to do proper
    joins as SQL calls with Artanis).
    
    This way, also, doesn't change as much of the code that's already written.

--- app/models/OBJECTS.scm
+++ app/models/OBJECTS.scm
@@ -3,7 +3,41 @@
 ;; This file is generated automatically by GNU Artanis.
 (create-artanis-model   ; DO NOT REMOVE THIS LINE!!!
   OBJECTS
-  (OBJECT_ID   auto       (#:unique))
-  (    AP_ID   text       (#:not-null))
-  (OBJECT_TYPE char-field (#:not-null #:maxlen 50))
-  (JSON        text       (#:not-null)))
+  (OBJECT_ID     auto        (#:unique))
+  (    AP_ID     text        (#:not-null))
+  (OBJECT_TYPE   char-field  (#:not-null #:maxlen 50))
+  ;;;; These can be a single or many relation
+  ;;;; I'll probably want to handle these with a linking table
+  ;; (ATTACHMENT    big-integer)
+  ;; (ATTRIBUTED_TO )
+  ;; (AUDIENCE      )
+  (CONTENT       text)
+  ;;;; The spec. says this is intentionally vague but that reads
+  ;;;; as "undefined", to me; we may leave this guy out until we
+  ;;;; determine if it's even needed
+  ;; (CONTEXT       )
+  (NAME          char-field  (#:maxlen 64))
+  (STARTTIME     date-field)
+  (  ENDTIME     date-field)
+  ;;;; Also vague; seems it stores any kind of object so we'll put
+  ;;;; an ID, here; we'll also not bother with it, for now
+  ;; (GENERATOR     big-integer)
+  ;;;; This one, at least, is specific but it can also have a list
+  ;;;; of values and I don't want to build this, yet
+  ;; (ICON          )
+  ;;;; Honestly, content was the main thing I wanted so I'm just
+  ;;;; going to add the rest of the possible properties here and
+  ;;;; implement them later
+  ;; (IMAGE            )
+  ;; (IN_REPLY_TO      )
+  ;; (LOCATION         )
+  ;; (PREVIEW          )
+  ;; (PUBLISHED        )
+  ;; (REPLIES          )
+  ;; (SUMMARY          )
+  ;; (TAG              )
+  ;; (UPDATED          )
+  ;; (URL              )
+  ;; (MEDIA_TYPE       )
+  ;; (DURATION         )
+  (JSON          text        (#:not-null)))

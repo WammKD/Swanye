01d3666b12b60596c02a9fbfae091616b2eff1ed
Sherlock Holmes <Wamm_KD_Schmelingski@yahoo.com>
2020-09-26 00:47:27 -0500

    I really need to figure out a way to make these easier to read and write.
    Maybe separate libraries that handle getting and setting and have a
    record object of each table to represent the objects they consist of.
    
    Anyway, this is just the start; turns out Artanis is transforming all
    double quotes into single quotes before `INSERT`ing them into the
    database. Probably more universally standard/safe; maybe.
    
    Which may be for the best, anyway; storing JSON in the database
    probably isn't good practice.
    
    I dunno; I'm tired.
    
    So we'll have to revise this (and probably use `NULL`s far more than I
    like).
    
    At this point, it may well be worth keeping Actors out of the
    `OBJECTS` table altogether; I'm mostly just repeating everything in
    the `OBJECTS` table in the `ACTORS` table (other than the `JSON`
    column; there's be good reason to add it, now).
    
    I can't specify the `OBJECTS` table any further since I don't know
    what type of Objects I might receive.
    
    But I know, specifically, what an Actor looks like so I can keep that
    specialized.
    
    I dunno; I do think there's quite a bit of redundancy but,
    technically, an Actor can have all properties available to any Object.
    
    Maybe I'd be better suited to keep things as they are and just split
    out the `OBJECTS` table into all the available properties and just use
    `NULL`s far more than I like.
    
    The thing I liked about storing (and, theoretically, converting to
    Scheme) the JSON was that it'd handle the lack of values natively, as
    Scheme typically does.
    
    Now, I've got to do weird checks; oh, well. It is what it is.

--- app/controllers/main.scm
+++ app/controllers/main.scm
@@ -3,7 +3,9 @@
 ;; This file is generated automatically by GNU Artanis.
 (define-artanis-controller main) ; DO NOT REMOVE THIS LINE!!!
 
-(use-modules (Swanye utils))
+(use-modules (app models   OBJECTS) (srfi srfi-26)
+             (app models  SESSIONS) (Swanye utils)
+             (app models TIMELINES))
 
 (get "/" #:with-auth "/auth/sign_in"
   (lambda (rc)
@@ -13,4 +15,32 @@
   (options #:with-auth "/auth/sign_in")
 
   (lambda (rc)
-    (view-render "main" (the-environment))))
+    (let ([posts ($OBJECTS
+                   'get
+                   #:columns   '(*)
+                   #:condition (where
+                                 #:OBJECT_ID
+                                 (assoc-ref
+                                   (car ($TIMELINES
+                                          'get
+                                          #:columns   '(OBJECT_ID)
+                                          #:condition (where
+                                                        #:USER_ID
+                                                        (assoc-ref
+                                                          (car ($SESSIONS
+                                                                 'get
+                                                                 #:columns   '(USER_ID)
+                                                                 #:condition (where
+                                                                               #:SESSION_ID
+                                                                               (car (assoc-ref
+                                                                                      (map
+                                                                                        (cut string-split <> #\=)
+                                                                                        (string-split
+                                                                                          (assoc-ref
+                                                                                            (request-headers (rc-req rc))
+                                                                                            'cookie)
+                                                                                          #\,))
+                                                                                      "sid")))))
+                                                          "USER_ID"))))
+                                   "OBJECT_ID")))])
+      (view-render "main" (the-environment)))))

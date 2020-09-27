bdb413c0c0ba157e1f9b3b4a575d128ce07922c3
Sherlock Holmes <Wamm_KD_Schmelingski@yahoo.com>
2020-09-26 23:37:24 -0500

    We need both the Object that was posted but also the Actor which posted it.
    This'll link 'em.

--- app/controllers/main.scm
+++ app/controllers/main.scm
@@ -3,8 +3,9 @@
 ;; This file is generated automatically by GNU Artanis.
 (define-artanis-controller main) ; DO NOT REMOVE THIS LINE!!!
 
-(use-modules (app models   OBJECTS) (srfi srfi-26)
-             (app models  SESSIONS) (Swanye utils)
+(use-modules (app models    ACTORS) (srfi srfi-26)
+             (app models   OBJECTS) (Swanye utils)
+             (app models  SESSIONS)
              (app models TIMELINES))
 
 (get "/" #:with-auth "/auth/sign_in"
@@ -15,32 +16,45 @@
   (options #:with-auth "/auth/sign_in")
 
   (lambda (rc)
-    (let ([posts ($OBJECTS
-                   'get
-                   #:columns   '(*)
-                   #:condition (where
-                                 #:OBJECT_ID
-                                 (assoc-ref
-                                   (car ($TIMELINES
-                                          'get
-                                          #:columns   '(OBJECT_ID)
-                                          #:condition (where
-                                                        #:USER_ID
-                                                        (assoc-ref
-                                                          (car ($SESSIONS
-                                                                 'get
-                                                                 #:columns   '(USER_ID)
-                                                                 #:condition (where
-                                                                               #:SESSION_ID
-                                                                               (car (assoc-ref
-                                                                                      (map
-                                                                                        (cut string-split <> #\=)
-                                                                                        (string-split
-                                                                                          (assoc-ref
-                                                                                            (request-headers (rc-req rc))
-                                                                                            'cookie)
-                                                                                          #\,))
-                                                                                      "sid")))))
-                                                          "USER_ID"))))
-                                   "OBJECT_ID")))])
+    (let ([postsAndCreator (map
+                             (lambda (post)
+                               (let ([ACTOR_ID (assoc-ref post "ATTRIBUTED_TO")])
+                                 (cons
+                                   post
+                                   (append
+                                     (car ($OBJECTS
+                                            'get
+                                            #:columns   '(*)
+                                            #:condition (where #:OBJECT_ID ACTOR_ID)))
+                                     (car ($ACTORS
+                                            'get
+                                            #:columns   '(*)
+                                            #:condition (where #:ACTOR_ID  ACTOR_ID)))))))
+                             ($OBJECTS
+                               'get
+                               #:columns   '(*)
+                               #:condition (where
+                                             #:OBJECT_ID
+                                             (map cdar ($TIMELINES
+                                                         'get
+                                                         #:columns   '(OBJECT_ID)
+                                                         #:condition (where
+                                                                       #:USER_ID
+                                                                       (assoc-ref
+                                                                         (car ($SESSIONS
+                                                                                'get
+                                                                                #:columns   '(USER_ID)
+                                                                                #:condition (where
+                                                                                              #:SESSION_ID
+                                                                                              (car (assoc-ref
+                                                                                                     (map
+                                                                                                       (cut string-split <> #\=)
+                                                                                                       (string-split
+                                                                                                         (assoc-ref
+                                                                                                           (request-headers
+                                                                                                             (rc-req rc))
+                                                                                                           'cookie)
+                                                                                                         #\,))
+                                                                                                     "sid")))))
+                                                                         "USER_ID")))))))])
       (view-render "main" (the-environment)))))

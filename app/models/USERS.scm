216ea5c2654a0e81efebb3b282aff2535be87edb
Sherlock Holmes <Wamm_KD_Schmelingski@yahoo.com>
2020-10-01 00:50:55 -0500

    And adjust the alignment of the rest of these.

--- app/models/USERS.scm
+++ app/models/USERS.scm
@@ -3,13 +3,12 @@
 ;; This file is generated automatically by GNU Artanis.
 (create-artanis-model   ; DO NOT REMOVE THIS LINE!!!
   USERS
-  (E_MAIL             char-field  (#:maxlen   255 #:not-null))
-  (PASSWORD           char-field  (#:maxlen   128 #:not-null))
-  (SALT               char-field  (#:maxlen   256 #:not-null))
-  (CREATED_AT         big-integer                (#:not-null))
-  (CONFIRMATION_TOKEN char-field  (#:maxlen   128 #:not-null))
-  ( PUBLIC_KEY        text                       (#:not-null))
-  (PRIVATE_KEY        text                       (#:not-null)))
-
   (USER_ID            auto                                (#:unique))
   (PREFERRED_USERNAME char-field  (#:maxlen  64 #:not-null #:unique))
+  (E_MAIL             char-field  (#:maxlen 255 #:not-null))
+  (PASSWORD           char-field  (#:maxlen 128 #:not-null))
+  (SALT               char-field  (#:maxlen 256 #:not-null))
+  (CREATED_AT         big-integer              (#:not-null))
+  (CONFIRMATION_TOKEN char-field  (#:maxlen 128 #:not-null))
+  ( PUBLIC_KEY        text                     (#:not-null))
+  (PRIVATE_KEY        text                     (#:not-null)))

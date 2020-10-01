d95e2862722c61bdb4bf1299495ad128f4375e5e
Sherlock Holmes <Wamm_KD_Schmelingski@yahoo.com>
2020-10-01 00:46:03 -0500

    Adjust alignment; also, preferred username isn't required so don't make `#:not-null`.

--- app/models/ACTORS.scm
+++ app/models/ACTORS.scm
@@ -3,11 +3,11 @@
 ;; This file is generated automatically by GNU Artanis.
 (create-artanis-model   ; DO NOT REMOVE THIS LINE!!!
   ACTORS
-  (ACTOR_ID           auto        (#:unique))
-  ( INBOX             text        (#:not-null))
-  (OUTBOX             text        (#:not-null))
+  (ACTOR_ID           auto       (#:unique))
+  ( INBOX             text       (#:not-null))
+  (OUTBOX             text       (#:not-null))
   (FOLLOWING          text)
   (FOLLOWERS          text)
   (LIKED              text)
   (FEATURED           text)
-  (PREFERRED_USERNAME char-field  (#:not-null #:maxlen 64))
+  (PREFERRED_USERNAME char-field (#:maxlen 64)))

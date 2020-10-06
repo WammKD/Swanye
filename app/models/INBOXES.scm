e2252d8bd9eec68da75a2877463192bb95b2a535
Sherlock Holmes <Wamm_KD_Schmelingski@yahoo.com>
2020-10-05 23:07:36 -0500

    Time to simplify this.
    We don't need the Activity type or Actor/Object IDs – those are all
    stored with the Activity. We just need to designate which user('s inbox) the
    particular activity belongs to.
    
    So I guess it's more of a mapping table; but that's O. K.
    
    This should, really, be all we need.

--- app/models/INBOXES.scm
+++ app/models/INBOXES.scm
@@ -3,10 +3,7 @@
 ;; This file is generated automatically by GNU Artanis.
 (create-artanis-model   ; DO NOT REMOVE THIS LINE!!!
   INBOXES
-  (       ID     auto        (#:unique))
-  (  USER_ID     big-integer (#:not-null))
-  ( ACTOR_ID     big-integer (#:not-null))
-  (OBJECT_ID     big-integer (#:not-null))
-  (ACTIVITY      text        (#:not-null))
-  (ACTIVITY_TYPE text        (#:not-null)))
+  (    USER_ID big-integer (#:not-null))
+  (ACTIVITY_ID big-integer (#:not-null))
+  #:primary-keys (USER_ID ACTIVITY_ID))
 

8b7253282f710f4437ab4c921d41a96bd8eddebf
Sherlock Holmes <Wamm_KD_Schmelingski@yahoo.com>
2020-10-06 19:28:59 -0500

    HDGGHHDHGHDHFHG

--- lib/Swanye/database-setters.scm
+++ lib/Swanye/database-setters.scm
@@ -275,7 +275,7 @@
     ;;                    #:headers `((Accept  . "application/ld+json")
     ;;                                (Profile . "https://www.w3.org/ns/activitystreams")))
     ;;                (json-string->scm (utf8->string httpBody)))])
-    ((if (list? activityPubID) car)
+    ((if (list? activityPubID) car identity)
       (map
         (lambda (apID)
           (let ([actorFilename (string-append/shared
